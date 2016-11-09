fe016fd23b265610922eef95830793df962d9073
Makaremko Dmitriy <makarenko991@gmail.com>
2016-11-09 14:18:14 +0200

    add classes

---
+++ Domain/Entities/Category.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Abstract;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    [Table("Categories")]
+    public class Category : BaseEntity
+    {
+        public String Name { get; set; }
+
+        public virtual List<Product> Products { get; set; }
+    }
+}
---
+++ Domain/Entities/Product.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Abstract;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    [Table("Products")]
+    public class Product : BaseEntity
+    {
+        public String Name { get; set; }
+
+        public virtual Category Category { get; set; }
+    }
+}
